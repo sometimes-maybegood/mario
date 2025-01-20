61aa4ac0c56b240ffdee362edd0c58cf2c817dbf
marat <maratakhun@gmail.com>
2025-01-20 21:32:01 +0300

    Ахунзянов, создание окна меню

--- game.py
+++ game.py
@@ -11,7 +11,6 @@ clock = pygame.time.Clock()
 font_path = 'mario_font.ttf'
 font_large = pygame.font.Font(font_path, 48)
 font_small = pygame.font.Font(font_path, 24)
-
 INIT_DELAY = 2000
 spawn_delay = INIT_DELAY
 DECREASE_BASE = 1.01
--- menu_qt.py
+++ menu_qt.py
@@ -6,3 +6,8 @@ class Ui_MenuWindow(object):
     def setupUi(self, MenuWindow):
         MenuWindow.setObjectName("MenuWindow")
         MenuWindow.resize(800, 800)
+
+        self.background = QtWidgets.QLabel(self.centralwidget)
+        self.background.setGeometry(0, 0, 800, 800)
+        self.background.setPixmap(QtGui.QPixmap("background.jpg"))
+        self.background.setScaledContents(True)
