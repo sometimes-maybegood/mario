e6f6d2016fb10e57c842bcc4c77b4b2e8534dfdb
Admin <chpunkpas09@gmail.com>
2025-01-20 19:51:20 +0300

    + нововведения

--- game.py
+++ game.py
@@ -12,3 +12,15 @@ font_path = 'mario_font.ttf'
 font_large = pygame.font.Font(font_path, 48)
 font_small = pygame.font.Font(font_path, 24)
 
+INIT_DELAY = 2000
+spawn_delay = INIT_DELAY
+DECREASE_BASE = 1.01
+last_spawn_time = pygame.time.get_ticks()
+
+game_over = False
+retry_text = font_small.render('PRESS ANY KEY', True, (255, 255, 255))
+retry_rect = retry_text.get_rect()
+retry_rect.midtop = (W // 2, H // 2)
+
+score = 0
+
