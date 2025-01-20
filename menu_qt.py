c76b633e328ef122bfb00ac11a4760a0b3ff4084
marat <maratakhun@gmail.com>
2025-01-20 21:57:16 +0300

    Ахунзянов, окно меню

--- menu_qt.py
+++ menu_qt.py
@@ -11,3 +11,9 @@ class Ui_MenuWindow(object):
         self.background.setGeometry(0, 0, 800, 800)
         self.background.setPixmap(QtGui.QPixmap("background.jpg"))
         self.background.setScaledContents(True)
+
+        self.lineEdit_start = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_start.setGeometry(QtCore.QRect(100, 100, 200, 30))
+        self.lineEdit_start.setObjectName("lineEdit_start")
+        self.lineEdit_start.setPlaceholderText("Начать")
+
