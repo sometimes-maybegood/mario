768c82d5e442d234e0e518185d9da83fadda56e7
Admin <chpunkpas09@gmail.com>
2025-01-20 19:49:24 +0300

    шрифт

--- game.py
+++ game.py
@@ -8,3 +8,6 @@ screen = pygame.display.set_mode((W, H))
 FPS = 60
 clock = pygame.time.Clock()
 
+font_path = 'mario_font.ttf'
+font_large = pygame.font.Font(font_path, 48)
+font_small = pygame.font.Font(font_path, 24)
