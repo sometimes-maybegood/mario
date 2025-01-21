969228e68775c727c3b7c36c06d6aaabadc15877
marat <maratakhun@gmail.com>
2025-01-21 17:57:58 +0300

    Ахунзянов, окно меню, commit 5

--- menu_qt.py
+++ menu_qt.py
@@ -16,10 +16,34 @@ class Ui_MenuWindow(object):
         self.pushButton_start = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_start.setGeometry(QtCore.QRect(350, 250, 300, 80))
         self.pushButton_start.setObjectName("pushButton_start")
-        self.pushButton_start.setPlaceholderText("Начать  игру")
+        self.pushButton_start.setPlaceholderText("Начать игру")
+        self.pushButton_start.setStyleSheet("""
+                    QPushButton {
+                        background-color: #4CAF50; /* Зеленый фон */
+                        color: white; /* Белый текст */
+                        border-radius: 10px; /* Закругленные углы */
+                        padding: 10px;
+                    }
+                    QPushButton:hover {
+                        background-color: #45a049; /* Темно-зеленый при наведении */
+                    }
+                """)
 
         self.pushButton_record = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_record.setGeometry(QtCore.QRect(350, 250, 200, 60))
         self.pushButton_record.setObjectName("pushButton_record")
         self.pushButton_record.setPlaceholderText("Рекорд")
+        self.pushButton_record.setStyleSheet("""
+                    QPushButton {
+                        background-color: #008CBA; /* Синий фон */
+                        color: white; /* Белый текст */
+                        border-radius: 10px; /* Закругленные углы */
+                        padding: 10px;
+                    }
+                    QPushButton:hover {
+                        background-color: #007B9E; /* Темно-синий при наведении */
+                    }
+                """)
+
+
 
