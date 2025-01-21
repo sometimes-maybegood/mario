be2d44f925df4b420d0b206045a0b2db88657529
marat <maratakhun@gmail.com>
2025-01-21 17:37:34 +0300

    Ахунзянов, окно меню

--- menu_qt.py
+++ menu_qt.py
@@ -8,12 +8,17 @@ class Ui_MenuWindow(object):
         MenuWindow.resize(800, 800)
 
         self.background = QtWidgets.QLabel(self.centralwidget)
-        self.background.setGeometry(0, 0, 800, 800)
+        self.background.setGeometry(0, 0, 1000, 800)
         self.background.setPixmap(QtGui.QPixmap("background.jpg"))
         self.background.setScaledContents(True)
 
         self.lineEdit_start = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_start.setGeometry(QtCore.QRect(100, 100, 200, 30))
+        self.lineEdit_start.setGeometry(QtCore.QRect(350, 250, 300, 80))
         self.lineEdit_start.setObjectName("lineEdit_start")
-        self.lineEdit_start.setPlaceholderText("Начать")
+        self.lineEdit_start.setPlaceholderText("Начать  игру")
+
+        self.lineEdit_record = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_record.setGeometry(QtCore.QRect(350, 250, 200, 60))
+        self.lineEdit_record.setObjectName("lineEdit_record")
+        self.lineEdit_record.setPlaceholderText("Рекорд")
 
