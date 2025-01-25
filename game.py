b8bc7ade3c4ce54143d788eefdb19910551449fa
sometimes-maybegood <maratakhun@gmail.com>
2025-01-26 00:24:45 +0300

    Ахунзянов земля

--- game.py
+++ game.py
@@ -158,6 +158,14 @@ while running:
     for i in range(int((W + ground_image.get_width()) / ground_image.get_width()) + 1):
         screen.blit(ground_image, (i * ground_image.get_width() - camera.x, H - GROUND_H - camera.y + 60))
 
+    for i in range(int((W + ground_image.get_width()) / ground_image.get_width()) + 1):
+        screen.blit(ground_image, (i * ground_image.get_width() - camera.x, H - GROUND_H - camera.y + 120))
+
+    for i in range(int((W + ground_image.get_width()) / ground_image.get_width()) + 1):
+        screen.blit(ground_image, (i * ground_image.get_width() - camera.x, H - GROUND_H - camera.y + 180))
+
+    for i in range(int((W + ground_image.get_width()) / ground_image.get_width()) + 1):
+        screen.blit(ground_image, (i * ground_image.get_width() - camera.x, H - GROUND_H - camera.y + 240))
 
     player.draw(screen, camera)
 
