6f7ce4f8ca178850edacd3968fee4cf5d1e157a8
sometimes-maybegood <maratakhun@gmail.com>
2025-01-26 21:15:07 +0300

    Ахунзянов

--- game.py
+++ game.py
@@ -7,17 +7,7 @@ W = 800
 H = 600
 screen = pygame.display.set_mode((W, H))
 
-FPS = 60
-clock = pygame.time.Clock()
-STEP = 10
-all_sprites = pygame.sprite.Group()
-font_path = 'mario_font.ttf'
-font_large = pygame.font.Font(font_path, 48)
-font_small = pygame.font.Font(font_path, 24)
-INIT_DELAY = 2000
-spawn_delay = INIT_DELAY
-DECREASE_BASE = 1.01
-last_spawn_time = pygame.time.get_ticks()
+вввввввв
 
 game_over = False
 retry_text = font_small.render('PRESS ANY KEY', True, (255, 255, 255))
