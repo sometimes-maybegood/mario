341dc7b26be4c9ce0c393d31aacf00f148e7f5aa
sometimes-maybegood <maratakhun@gmail.com>
2025-01-31 22:58:46 +0300

    Ахунзянов, регистрация

--- register.py
+++ register.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import sqlite3
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QMessageBox
 from login import Ui_LoginWindow
@@ -25,12 +26,33 @@ class LoginApp(QtWidgets.QMainWindow):
     def register(self):
         username = self.ui.lineEdit_username.text()
         password = self.ui.lineEdit_password.text()
-        print(f"Регистрация: {username}, Пароль: {password}")
+
+        if not username or not password:
+            self.show_error_message("Ошибка", "Логин и пароль не должны быть пустыми.")
+            return
+
+        conn = sqlite3.connect('users.db')
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+            conn.commit()
+            print(f"Регистрация успешна: {username}")
+            self.open_menu()
+        except sqlite3.IntegrityError:
+            self.show_error_message("Ошибка", "Пользователь с таким логином уже существует.")
+        finally:
+            conn.close()
 
     def login(self):
         username = self.ui.lineEdit_username.text()
         password = self.ui.lineEdit_password.text()
         print(f"Вход: {username}, Пароль: {password}")
+
+    def open_menu(self):
+        self.menu_window = MenuApp()
+        self.menu_window.show()
+        self.close()
 
 
 def main():
