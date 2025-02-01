ce11fef506888f369711a883c134d59375ee48dd
sometimes-maybegood <maratakhun@gmail.com>
2025-02-01 20:42:38 +0300

    Ахунзянов

---
+++ scores.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def create_database():
+    conn = sqlite3.connect('scores.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS records (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            score INTEGER NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    create_database()
