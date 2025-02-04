a8c168f2bbdaf56f0ef5964be23781a9851e7be8
marat <maratakhun@gmail.com>
2025-02-04 23:32:06 +0300

    Ахунзянов

--- users.py
+++ users.py
@@ -6,7 +6,7 @@ def create_database():
     cursor = conn.cursor()
 
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS scores (
+        CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             username TEXT NOT NULL,
             password TEXT NOT NULL
